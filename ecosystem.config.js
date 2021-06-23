662d1c6b5a6f1dde817c3f3b4f45764d9c98455c
programando <jhonjamesmg@hotmail.com>
2021-06-23 17:42:12 -0500

    ecosystem modify

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,12 @@ module.exports = {
       name: 'Pagos',
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
+      watch: ["server", "client"],
+      watch_delay: 1000,
+      ignore_watch : ["node_modules"],
+      watch_options: {
+        "followSymlinks": false
+      },
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start'
     }
