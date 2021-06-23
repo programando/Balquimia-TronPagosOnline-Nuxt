7af9bdb99ed549c64048e1055454ae10c2abdde2
programando <jhonjamesmg@hotmail.com>
2021-06-23 09:51:04 -0500

    Pagos

--- mixins/payuLatamForm.js
+++ mixins/payuLatamForm.js
@@ -4,15 +4,15 @@
     methods: {
         
         PayuLatamSendForm(form, keyField, md5Key) {
-            let valorPIN = parseInt(form.valor_pagar.replace(/\./g, '') ) ;
-            
+    
+        let valorPIN = parseInt(form.valor_pagar.replace(/\./g, ''));    
             document.getElementById('amount').value        = valorPIN;
             document.getElementById('buyerEmail').value    = form.email.trim();
-            document.getElementById('description').value   =  keyField;
+            document.getElementById('description').value   = keyField;
             document.getElementById('payerDocument').value = form.nro_identif.trim();
             document.getElementById('payerFullName').value = form.nom_full.trim();
-            document.getElementById('referenceCode').value = form.referenceCode.trim();
-            document.getElementById('signature').value = md5Key;
+            document.getElementById('referenceCode').value = keyField;
+            document.getElementById('signature').value     = md5Key;
             document.psePayForm.submit();     
         },
       
